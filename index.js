6565e346da1b50c8023fcf21b6eed58d7d76f9f8
bex-ford <becky782@hotmail.co.uk>
2023-02-24 10:42:27 +0000

    added array of questions

--- index.js
+++ index.js
@@ -10,6 +10,139 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Array of questions for manager
+const managerPrompt = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?",
+    },
+  ]);
+};
 
+// Array of questions for employee 
+const employeePrompt = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's GitHub username?",
+    },
+  ]);
+};
+
+// Array of questions for employee 
+const employee1Prompt = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+      },
+    ]);
+  };
+
+  // Array of questions for employee 
+  const employee2Prompt = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+      },
+    ]);
+  };
+
+  // Array of questions for intern 
+  const internPrompt = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the intern's GitHub username?",
+      },
+    ]);
+  };
+
+  const createHTMLFile = (htmlPage) => {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+  }
